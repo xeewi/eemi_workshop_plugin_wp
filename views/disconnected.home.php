3b8c12ac6c6d990edd5c02d208965e05003ad38a
deskover <guillaume@deskover.co>
2016-10-19 04:54:58 +0200

    init steps

--- views/disconnected.home.php
+++ views/disconnected.home.php
@@ -1,21 +1,29 @@
 <div class="wrap" id="wpSM">
 
-	<h2>Slack Manager</h2>
+	<h2><i class="fa fa-slack" aria-hidden="true"></i> Slack Manager</h2>
 	
 	<div class="card">
 		<h3><?php echo __( 'Step 1 : Create your app', "wpSlackManager" ); ?></h3>
-
+		<p class="ps"><?php echo __( '* If you already got a Slack APP, you can skip this Step and directly go to Step 2', 'wpSlackManager' ); ?></p>
 		<p><?php echo __( 'You need a Slack APP to use Slack Manager.', 'wpSlackManager' ); ?></p>
-		<p><?php echo __( 'If you already got a Slack APP, you can skip this Step and directly got to Step 2', 'wpSlackManager' ); ?></p>
 		<p><?php echo __( 'Simply create it by clicking on the following link, and get yours Client ID and Client Secret. After creating your app, come back here for Step 2.', 'wpSlackManager' ); ?></p>
 		<p><?php echo __( 'Click here to create your Slack App !', "wpSlackManager" ); ?></p>
-		<p><a target="_blank" class="btn" href="https://api.slack.com/apps?new_app=1"> <i class="fa fa-slack" aria-hidden="true"></i> <?php echo __('Create an APP', "wpSlackManager" ); ?></a></p>
+		<p class="action">
+			<a target="_blank" class="btn" href="https://api.slack.com/apps?new_app=1"> 
+				<i class="fa fa-slack" aria-hidden="true"></i> 
+				<?php echo __('Create App', "wpSlackManager" ); ?>
+			</a>
+		</p>
 	</div>
 
 	<div class="card">
 		<h3><?php echo __( "Step 2 : Add yours Client ID and Client Secret" ,'wpSlackManager' ); ?></h3>
-		<form method="POST" >
-
+		<form method="POST" action="admin.php">
+			<label for="client_id">Client ID</label>
+			<input type="text" name="client_id" class="small" value="<?php echo $this->_token->client_id(); ?>">
+			<label for="client_secret">Client Secret</label>
+			<input type="text" name="client_secret" class="small" value="<?php echo $this->_token->client_secret(); ?>">
+			<p class="action"><button action="submit" class="btn" >Edit</button></p>
 		</form>
 	</div>
 
