578c1d7003de6e48448160e023072deef78c125e
deskover <guillaume@deskover.co>
2016-10-20 22:45:44 +0200

    add IM scope

--- wpSM.class.php
+++ wpSM.class.php
@@ -94,7 +94,8 @@ class wpSM {
 		"groups:read,groups:write," . 
 		"team:read," .
 		"users.profile:read,users.profile:write," .
-		"users:read,users:write";
+		"users:read,users:write," . 
+		"im:read,im:write,im:history";
 
 
 		require_once( WP_PLUGIN_DIR . '/wpSlackManager/views/auth.home.php' );
