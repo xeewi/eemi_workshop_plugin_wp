208910e069ce7329c0e8f0079cdf2a17fc7f58c1
deskover <guillaume@deskover.co>
2016-10-21 15:39:18 +0200

    plugin v

--- wpSlackManager.php
+++ wpSlackManager.php
@@ -3,7 +3,7 @@
 * Plugin Name: wpSlackManager
 * Plugin URI: https://github.com/xeewi/eemi_workshop_plugin_wp
 * Description: Manage your Slack team from your WordPress dashboard
-* Version: 0.1
+* Version: 0.4
 * Author: Xeewi
 * Author URI: https://github.com/xeewi/
 * License: WTFPL
